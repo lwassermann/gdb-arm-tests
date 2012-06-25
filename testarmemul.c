1e3dd0c4062491699d4a8a1f2f9946d94f390e74
Lars Wassermann <lars.wassermann@googlemail.com>
2012-06-25 22:26:18 +0200

    changed the printing output and removed the pc variable.

--- testarmemul.c
+++ testarmemul.c
@@ -8,7 +8,7 @@
 void
 print_processor(ARMul_State* state)
   {
-    fprintf(stdout, "pc: 0x%p\tr0: %i\tr1: %i\tr2: %i\tr3: %i\n    insn: 0x%x\tdec: 0x%x load: 0x%x\tNextInsn: %i\tpc-val: 0x%x\tErr: %i\n",
+    fprintf(stdout, "pc: 0x%p\tr0: %.8i\tr1: %.8i\tr2: %.8i\tr3: %.8i\n    insn: 0x%.8x\tdec: 0x%.8x load: 0x%.8x\tNextInsn: %i\tpc-val: 0x%.8x\tErr: %i\n",
     	state->Reg[15],
     	ARMul_GetReg(state, state->Mode, 0),
     	ARMul_GetReg(state, state->Mode, 1),
@@ -18,7 +18,7 @@ print_processor(ARMul_State* state)
     	state->decoded,
     	state->loaded,
     	state->NextInstr,
-    	ARMul_ReadWord(state, ARMul_GetPC(state)-8),
+    	ARMul_ReadWord(state, ARMul_GetPC(state)),
     	state->ErrorCode);
   }
 
@@ -26,12 +26,12 @@ int
 main ()
   {
     int i, j;
-    ARMword data[3] = {0xE3A01080, 
-    		       0xE2810001,
-    		       0xE1B01001};
+    ARMword data[4] = { 0xE3A01080, 
+    			0xE2810001,
+    			0xE1B01001,
+    			0x0};
     ARMul_State* state;
-    ARMword pc = 0;
-
+    
     
     ARMul_EmulateInit ();
     state = ARMul_NewState ();
@@ -40,19 +40,15 @@ main ()
     //ARMul_MemoryInit (state, 1<<21); // mem_size copied from gdb armul wrapper
     //ARMul_OSInit (state);
     
-    state->verbose = 4;
     ARMul_SetPC (state, (ARMword) &data);
     
     print_processor(state);
-    pc = ARMul_DoInstr(state);
-    fprintf(stdout, "0x%p\n", pc);
+    state->Reg[15] = ARMul_DoInstr(state);
     print_processor(state);
     
     state->NextInstr = RESUME;
-    ARMul_SetPC (state, pc);
     
-    pc = ARMul_DoInstr(state);
-    fprintf(stdout, "0x%p\n", pc);
+    state->Reg[15] = ARMul_DoInstr(state);
     print_processor(state);
     //ARMul_DoInstr(state);
     //print_processor(state);
